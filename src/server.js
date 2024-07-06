f9a4eb6ef9a21289fcdcd5b48a27359445982efa
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-07-06 23:48:42 +0700

    #18

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express')  //commonjs
+const path = require('path');
+
+const app = express()   //app express
+const port = 2003   //port
+
+
+//config template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+// Khai báo route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/homepage', (req, res) => {
+    // res.send('Chào mừng đến với trang củ của Vương Nam')
+    res.render('sample.ejs')
+})
+
+app.get('/h1text', (req, res) => {
+    res.send('<h1> Chào mừng đến với trang củ của Vương Nam <h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
