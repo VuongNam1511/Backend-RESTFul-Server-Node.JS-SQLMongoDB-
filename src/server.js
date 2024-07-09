c803feac3c8145873024f473fa6c2723f74d0022
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-07-09 21:33:50 +0700

    #30

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require('express')  //commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 
+const mysql = require('mysql2');
+
 const { config } = require('dotenv');
 const app = express()   //app express
 const port = process.env.PORT || 8887;   //port  => hardcode .   uat .   prod
@@ -13,6 +15,27 @@ configViewEngine(app);
 
 // khai bao routes
 app.use('/', webRoutes);
+
+// Test conection
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, //default: 3306
+    user: 'root', //default: empty
+    password: '123456',
+    database: 'hoidanit',
+});
+
+// A simple query
+
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, results, fields) {
+        console.log(">>>results= ", results); // results contains rows returned by server
+        console.log(">>>fields= ", fields); // fields contains extra meta data about results, if available
+    }
+);
+
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
