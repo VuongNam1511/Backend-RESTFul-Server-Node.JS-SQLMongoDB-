dba2dfc08a4f1cca2d9d79f53cf2d868fc4fd209
Vương Nam <106067360+VuongNam1511@users.noreply.github.com>
2024-07-12 21:05:11 +0700

    #32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,22 @@
+const connection = require('../config/database');
 
 const getHomepage = (req, res) => {
     // process data
     // call model
-    res.send('Hello World! after install nodemon')
+    let users = [];
+
+    connection.query(
+        'SELECT * FROM Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>results home page= ", results); // results contains rows returned by server
+
+            // console.log(">>>check users: ", users)
+            res.send(JSON.stringify(users))
+        }
+    );
+
+
 }
 const getH1text = (req, res) => {
     res.send('<h1> Chào mừng đến với trang chủ của Vương Nam <h1>')
